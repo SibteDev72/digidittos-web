3fc09fd3f2b54ec9342bbeb3d3962ff3bec60dc8
SibteDev72 <sibte.dev72@gmail.com>
2025-06-13 20:18:18 +0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full flex flex-col gap-2">
+    <BarNavbar />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+// const showNavFooter = computed(() => {
+//   const path = route.path;
+//   return path === "/success";
+// });
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-primary2">
+        <p class="text-white">HomePage</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'default'
+})
+</script>
+
+<style scoped>
+
+</style>
